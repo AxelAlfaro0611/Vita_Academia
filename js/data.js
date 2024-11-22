132377166c892e95202a48b0d8e22d43719ef2c9
Luis LT <105554066+LTprograms@users.noreply.github.com>
2024-11-21 19:47:59 -0500

    data estructurada de ciclos v1

--- js/data.js
+++ js/data.js
@@ -21,3 +21,82 @@ const servicios = [
         title: 'aula_virtual',
     },
 ]
+
+const universidades = {
+    "unmsm" : [
+        "San Marcos",
+        "Universidad Nacional Mayor de San Marcos"
+    ],
+    "unfv" : [
+        "Villarreal",
+        "Universidad Nacional Federico Villarreal"
+    ],
+    "unac" : [
+        "Callao",
+        "Universidad Nacional del Callao"
+    ],
+    "uni" : [
+        "UNI",
+        "Universidad Nacional de Ingeniería"
+    ],
+    "unalm" : [
+        "Agraria",
+        "Universidad Nacional Agraria la Molina"
+    ],
+}
+
+const arreglo_ciclos = [
+    {
+        universidades: [
+            "unmsm",
+        ],
+        ciclos: [
+            {
+                tipo: "verano",
+            },
+            {
+                tipo: "repaso",
+            },
+        ]
+    },
+    {
+        universidades: [
+            "unfv",
+        ],
+        ciclos: [
+            {
+                tipo: "verano",
+            },
+            {
+                tipo: "repaso",
+            },
+        ]
+    },
+    {
+        universidades: [
+            "unac",
+            "uni"
+        ],
+        ciclos: [
+            {
+                tipo: "verano",
+            },
+            {
+                tipo: "repaso",
+            },
+        ]
+    },
+    {
+        universidades: [
+            "unalm",
+        ],
+        ciclos: [
+            {
+                tipo: "verano",
+            },
+            {
+                tipo: "repaso",
+            },
+        ]
+    },
+]
